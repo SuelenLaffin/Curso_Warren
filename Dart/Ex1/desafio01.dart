d909ab8c5feb900fe34731e95567e33f12b10653
Suelen Laís Laffin <Suelen.Laffin@proway.treina>
2022-06-30 11:03:27 -0300

    [change]

--- Dart/Ex1/desafio01.dart
+++ Dart/Ex1/desafio01.dart
@@ -4,59 +4,38 @@
 
 // calculo para descobrir o imc
 
+import 'dart:io';
 
-
-import 'dart:io'; 
-void main(){
-
+void main() {
   print('Qual seu nome: ');
   String? nome = stdin.readLineSync();
 
   print('Digite seu peso: ');
   double? peso = double.parse(stdin.readLineSync()!);
 
-  print('Digite sua altura: ');
-  double? altura = double.parse(stdin.readLineSync()!);
+  if (peso > 200) {
+    print('Peso inválido!');
+  } else {
+    print('Digite sua altura: ');
+    double? altura = double.parse(stdin.readLineSync()!);
 
-  double calculoIMC = peso /(altura*altura) ;
+    if (altura > 2.20) {
+      print('Altura inválida');
+    } else {
+      double calculoIMC = peso / (altura * altura);
+      var IMCformatado = calculoIMC.toStringAsFixed(2);
 
-  var IMCformatado =calculoIMC.toStringAsFixed(2); 
-
-  
-
-    while(altura < 2.30 && peso < 200){
-
-      if (calculoIMC < 18.5){
+      if (calculoIMC < 18.5) {
         print('$nome, seu IMC é $IMCformatado e sua classificação é MAGREZA');
-        
-        break;
-        
-
-      } else if(calculoIMC > 18.5 &&  calculoIMC < 24.9){
+      } else if (calculoIMC > 18.5 && calculoIMC < 24.9) {
         print('$nome, seu IMC é $IMCformatado  e sua classificação é NORMAL');
-
-        break;
-
-      } else if(calculoIMC > 25 && calculoIMC <29.9){
-        print('$nome, seu IMC é $IMCformatado  e sua classificação é SOBREPESO');
-
-        break;
-        
-      } else if(calculoIMC > 30){
-        print('$nome, seu IMC é $IMCformatado  e sua classificação é OBESIDADE');
-
-        break;
-
-      } else {
-        print('Peso ou altura inválida');
-
-        break;
+      } else if (calculoIMC > 25 && calculoIMC < 29.9) {
+        print(
+            '$nome, seu IMC é $IMCformatado  e sua classificação é SOBREPESO');
+      } else if (calculoIMC > 30) {
+        print(
+            '$nome, seu IMC é $IMCformatado  e sua classificação é OBESIDADE');
       }
-
-      
     }
-  
-
-  
-  
+  }
 }
