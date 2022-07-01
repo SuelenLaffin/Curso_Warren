581f45cc10aa5c3e4b2f3be54ae489ac73a41a0d
Suelen Laís Laffin <Suelen.Laffin@proway.treina>
2022-07-01 09:18:49 -0300

    conteudo

--- Dart/Ex1/metodos.dart
+++ Dart/Ex1/metodos.dart
@@ -6,6 +6,26 @@ void main(){
 
   List<String> nomes = ['Suelen', 'Maria', 'Gabrielle'];
   nomes.shuffle();
-  nomes.add('Ana');
-  print(nomes);
+  nomes.add('Ana'); // adiciona um valor a lista
+  //nomes.remove('Maria'); // remove um valor na lista
+  //print('A lista está vazia? ${nomes.isEmpty}'); // vai dizer se a lista está vazia, retorna um valor bool
+
+  String nome = 'Ailton';
+  List <String> nomesAux = nomes.reversed.toList(); // para reverter os valores da lista
+  print(nomesAux);
+
+  print(nomes.any((value){
+    return value == 'Ailton';
+  }));
+  nomes.any((value)=> value == 'Ailton');
+  
+  nomes.contains('Ailton');
+
+
+
+Set meuSet = {'Ailton','Joaquim'};
+print(meuSet);
+
+
+
 }
---
+++ Dart/Ex1/operadores.dart
@@ -0,0 +1,46 @@
+
+void main(){
+
+double num1 = 10;
+double num2 = 10;
+
+//OPERADORES 
+
+// Soma + 
+//print(' A soma é ${num1 + num2}');
+
+
+//Subtração - 
+//print(' A subtração é ${num1 - num2}');
+
+
+//Multiplicação *
+//print(' A multiplicação ${num1 * num2}');
+
+
+//Divisão /
+//print(' A divisão é ${num1 / num2}');
+
+//Divide e retorna a parte inteira do resultado ~/
+//print(' A parte inteira da divisão é ${num1 ~/ num2}');
+
+//Retorna o resto da divisão %
+//print(' O resto da divisão é ${num1 % num2}');
+
+
+//OPERADORES LÓGICOS
+
+print(num1 > num2); // maior que
+print(num1 >= num2); // maior ou igual 
+print(num1 < num2); // menor que
+print(num1 <= num2); // menor ou igual
+print(num1 == num2); // Igual igual
+print(num1 != num2); // diferente que
+
+
+//ATRIBUIÇÃO
+
+
+
+}
+
